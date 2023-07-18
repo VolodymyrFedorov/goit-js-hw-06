2d14dfc969bbad0fa94703c706a8682ae5537919
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-07-18 14:34:36 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement('li');
+
+  listItem.textContent = ingredient;
+
+  listItem.classList.add('item');
+
+  fragment.appendChild(listItem);
+});
+
+ingredientsList.appendChild(fragment);
+
+console.log(ingredients);
