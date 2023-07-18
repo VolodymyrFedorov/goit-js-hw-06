95713e099d7befbad1e53ac6dbcf71695d209917
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-07-18 17:10:55 +0300

    8-th, 9-th tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Please fill in all fields');
+  } else {
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    console.log(formData);
+    loginForm.reset();
+  }
+});
 
